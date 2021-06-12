ed6140ceb321e7d34c32202349c42d3f64275e22
Lourdes <lourdeslunaweb@gmail.com>
2021-06-12 20:09:36 -0300

    Ejercicio 6 realizado

---
+++ Ejercicio 06 Remover Duplicados/script.js
@@ -0,0 +1,19 @@
+// removerDuplicados(array)
+// Crear una función removerDuplicados que tome como argumento un array array y que 
+// devuelva un array con los mismos valores de array pero sin valores duplicados.
+
+// removerDuplicados([1, 1, 1]) // [1]
+// removerDuplicados([1, 1, 2, 2, 3, 3]) // [1, 2 ,3]
+// removerDuplicados([5, 23, 8, 5, 5, 44, 23]) // [5, 23 ,8, 44]
+
+const removerDuplicados = (array) =>{
+
+    const arrayNuevo = [...new Set(array)]
+
+    return arrayNuevo
+
+}
+
+console.log(removerDuplicados([1, 1, 1]));
+console.log(removerDuplicados([1, 1, 2, 2, 3, 3]));
+console.log(removerDuplicados([5, 23, 8, 5, 5, 44, 23]));
