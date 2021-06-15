6f8dc58f845c87d952f29bde0b615d47b1b343aa
Lourdes <lourdeslunaweb@gmail.com>
2021-06-15 16:18:40 -0300

    actualizacion general

---
+++ Ejercicio 07 Repetir Letras/script.js
@@ -0,0 +1,28 @@
+// repetirLetras(palabra, cantidad)
+// Crear una función repetirLetras que tome como argumento un string palabra y un número 
+// entero cantidad, y devuelva una string donde cada letra de palabra esté repetida cantidad de veces.
+
+// repetirLetras('hola', 2) // 'hhoollaa'
+// repetirLetras('ada', 3) // 'aaadddaaa'
+// repetirLetras('ah!', 5) // 'aaaaahhhhh!!!!!'
+// repetirLetras('basta', 1) // 'basta'
+
+const repetirLetras = (palabra, cantidad) =>{
+
+    // let palabraArray = Array.from(palabra);
+
+    let palabrasRepetidas = palabra.repeat(cantidad)
+
+
+    return palabrasRepetidas
+    
+
+
+
+
+}
+
+console.log(repetirLetras('hola', 2));
+console.log(repetirLetras('ada', 3));
+console.log(repetirLetras('ah!', 5));
+console.log(repetirLetras('basta', 1));
